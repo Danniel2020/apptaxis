59d3f16916f4ca36cf3b9d70469fd1d0ec355861
Danniel <danielbeta19@gmail.com>
2023-03-24 23:21:04 -0500

    Correcion bugs

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express = require ('express')
-const enev = require ('dotenv')
-const cokiesParser = require('cookie-parser')
+const express = require('express')
+const dotenv = require('dotenv')
+const cokiesParser = require ('cookie-parser')
 
-const app = exprees()
-// stear eñmotor de la plantilla
-app.set ('view','ejs')
-//stiar la carpeta publica
-app.user(express.static('public'))
+const app = express()
 
-//configurar node para procesar los formularios
-app.user(express.urlencoded({extended:true}))
-app,use(express.json())
+//Setear el motor de plantilla
+app.set('view engine', 'ejs')
 
-//stear las variables
-dotenv.config({path:'./env/.env'})
+//Setear la carpeta public
+app.use(express.static('public'))
+
+//Configurar node para procesar datos de formularios
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//setear variables de entorno
+dotenv.config({path: './env/.env'})
 
 
-// llamar el enrutador
+//llamar al enrutador
+app.use('/',require('./routes/router.js'))
 
-app.user('/',require('./routes/routes.js'))
 
-app.listen(3000,()=>{
+app.listen(3000,()=> {
     console.log('El servidor esta corriendo en http://localhost:3000')
 })
-
---
+++ routes/router.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+
+// ruta para el home
+router.get('/', (req, res)=>{
+    // conexion()
+    res.render('index')
+})
+
+// ruta para el login
+router.get('/login', (req, res)=>{
+    res.render('login')
+})
+// ruta para el register
+router.get('/register', (req, res)=>{
+    res.render('register')
+})
+
+module.exports = router
